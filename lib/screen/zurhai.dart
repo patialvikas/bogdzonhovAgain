6f7bc151183d1818d4f7f2e4f3d6aa452ad30f91
vikas <unstopablevikas@outlook.com>
2022-01-31 11:59:55 +0530

    zasal integrated

--- lib/screen/zurhai.dart
+++ lib/screen/zurhai.dart
@@ -45,6 +45,10 @@ class _ZurhaiState extends State<Zurhai> {
     super.dispose();
   }
 
+  bool isThirdApiData=false;
+  String tailbar_data="";
+  String zasal_data="";
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -84,191 +88,208 @@ class _ZurhaiState extends State<Zurhai> {
     double totalHeight = MediaQuery.of(context).size.height;
     double totalWidth = MediaQuery.of(context).size.width;
     bool checkedBox = false;
-    return Stack(children: <Widget>[
-      Padding(
-        padding: EdgeInsets.only(left: totalWidth * 0.1, top: totalHeight * 0),
-        child: Container(
-          // color: Color.fromARGB(50, 200, 50, 20),
-          child: Column(
-            children: <Widget>[
-              Padding(
-                padding: EdgeInsets.only(
-                    left: totalWidth * 0, top: totalHeight * 0.15),
-                child: Image.asset('assets/budd.png', width: 150),
-              ),
-              Container(child: Text("Төрсөн өдөр")),
-              Container(
-                margin: EdgeInsets.only(top: 10),
-                child: Padding(
-                  padding: EdgeInsets.only(
-                      left: totalWidth * 0, top: totalHeight * 0),
-                  child: SizedBox(
-                    height: 100,
-                    width: totalWidth * 0.8,
-                    child: TextFormField(
-                      controller: myBirtController,
-                      onTap: () {
-                        FocusScope.of(context).requestFocus(new FocusNode());
-
-                        DatePicker.showDatePicker(context,
-                            showTitleActions: true,
-                            minTime: DateTime(1911, 3, 5),
-                            maxTime: DateTime(2022, 1, 7), onCancel: () {
-                          //set toast message here
-                          myBirtController.text = "";
-                        }, onChanged: (date) {
-                          var newFormat = DateFormat("yyyy-MM-dd");
-                          String updatedDt = newFormat.format(date);
-                          print(updatedDt);
-                          myBirtController.text = updatedDt.toString();
-                          print('change $date');
-                        }, onConfirm: (date) {
-                          var newFormat = DateFormat("yyyy-MM-dd");
-                          String updatedDt = newFormat.format(date);
-                          print(updatedDt);
-                          myBirtController.text = updatedDt.toString();
-                          print('confirm $date');
-                        }, currentTime: DateTime.now(), locale: LocaleType.en);
-                      },
-                      keyboardType: TextInputType.number,
-                      decoration: InputDecoration(
-                        hintText: 'Төрсөн он',
-                        hintStyle: TextStyle(color: Colors.black),
-                        filled: true,
-                        fillColor: Color(0xFFF8FAFB),
-                        enabledBorder: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(15),
-                            borderSide: BorderSide(color: Color(0xFFB4091E))),
-                        focusedBorder: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(15),
-                            borderSide: BorderSide(color: Color(0xFFB4091E))),
-                      ),
-                      validator: (bdayValue) {
-                        if (bdayValue!.isEmpty) {
-                          return 'Please enter email';
-                        }
-                        bday = bdayValue;
-                        return null;
-                      },
-                      cursorColor: Color(0xFF30C6CC),
-                    ),
-                  ),
-                ),
-              ),
-            ],
-          ),
-        ),
-      ),
-      Padding(
-        padding: const EdgeInsets.all(10.0),
-        child: Form(
-          key: _formKey,
-          child: Padding(
-            padding:
-                EdgeInsets.only(left: totalWidth * 0.1, top: totalHeight * 0.4),
+    return SingleChildScrollView(
+      child: Container(
+        child: Stack(children: <Widget>[
+          Padding(
+            padding: EdgeInsets.only(left: totalWidth * 0.1, top: totalHeight * 0),
             child: Container(
-              margin: EdgeInsets.only(top: 30),
               // color: Color.fromARGB(50, 200, 50, 20),
               child: Column(
                 children: <Widget>[
-                  Container(child: Text("Хүйс нь")),
+                  Padding(
+                    padding: EdgeInsets.only(
+                        left: totalWidth * 0, top: totalHeight * 0.15),
+                    child: Image.asset('assets/budd.png', width: 150),
+                  ),
+                  Container(child: Text("Төрсөн өдөр")),
                   Container(
                     margin: EdgeInsets.only(top: 10),
                     child: Padding(
                       padding: EdgeInsets.only(
                           left: totalWidth * 0, top: totalHeight * 0),
                       child: SizedBox(
-                        height: 60,
+                        height: 100,
                         width: totalWidth * 0.8,
-                        child:
+                        child: TextFormField(
+                          controller: myBirtController,
+                          onTap: () {
+                            FocusScope.of(context).requestFocus(new FocusNode());
 
-                            /* TextFormField(
-                      keyboardType: TextInputType.number,
-                      decoration: InputDecoration(
-                        hintText: 'Хүйс',
-                        hintStyle: TextStyle(color: Colors.black),
-                        filled: true,
-                        fillColor: Color(0xFFF8FAFB),
-                        enabledBorder: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(15),
-                            borderSide: BorderSide(color: Color(0xFFB4091E))),
-                        focusedBorder: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(15),
-                            borderSide: BorderSide(color: Color(0xFFB4091E))),
-                      ),
-                      cursorColor: Color(0xFF30C6CC),
-                    ),*/
-                            Container(
-                          decoration: BoxDecoration(
-                            border: Border.all(
-                                width: 1.0, color: Color(0xFFB4091E)),
-                            borderRadius: BorderRadius.all(Radius.circular(
-                                    15.0) //                 <--- border radius here
-                                ),
+                            DatePicker.showDatePicker(context,
+                                showTitleActions: true,
+                                minTime: DateTime(1911, 3, 5),
+                                maxTime: DateTime(2022, 1, 7), onCancel: () {
+                              //set toast message here
+                              myBirtController.text = "";
+                            }, onChanged: (date) {
+                              var newFormat = DateFormat("yyyy-MM-dd");
+                              String updatedDt = newFormat.format(date);
+                              print(updatedDt);
+                              myBirtController.text = updatedDt.toString();
+                              print('change $date');
+                            }, onConfirm: (date) {
+                              var newFormat = DateFormat("yyyy-MM-dd");
+                              String updatedDt = newFormat.format(date);
+                              print(updatedDt);
+                              myBirtController.text = updatedDt.toString();
+                              print('confirm $date');
+                            }, currentTime: DateTime.now(), locale: LocaleType.en);
+                          },
+                          keyboardType: TextInputType.number,
+                          decoration: InputDecoration(
+                            hintText: 'Төрсөн он',
+                            hintStyle: TextStyle(color: Colors.black),
+                            filled: true,
+                            fillColor: Color(0xFFF8FAFB),
+                            enabledBorder: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(15),
+                                borderSide: BorderSide(color: Color(0xFFB4091E))),
+                            focusedBorder: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(15),
+                                borderSide: BorderSide(color: Color(0xFFB4091E))),
                           ),
-                          child: DropdownButton(
-                            underline: Container(),
-                            hint: _dropDownValue == null
-                                ? Text('Dropdown')
-                                : Text(
-                                    _dropDownValue,
-                                    style: TextStyle(color: Colors.blue),
-                                  ),
-                            isExpanded: true,
-                            iconSize: 30.0,
-                            style: TextStyle(color: Colors.blue),
-                            items: ['Эрэгтэй', 'Эмэгтэй'].map(
-                              (val) {
-                                return DropdownMenuItem<String>(
-                                  value: val,
-                                  child: Text(val),
-                                );
-                              },
-                            ).toList(),
-                            onChanged: (val) {
-                              setState(
-                                () {
-                                  _dropDownValue = val as String;
-                                },
-                              );
-                            },
-                          ),
+                          validator: (bdayValue) {
+                            if (bdayValue!.isEmpty) {
+                              return 'Please enter email';
+                            }
+                            bday = bdayValue;
+                            return null;
+                          },
+                          cursorColor: Color(0xFF30C6CC),
                         ),
                       ),
                     ),
                   ),
-                  Container(
-                    margin: EdgeInsets.only(top: 30),
-                    child: Padding(
-                      padding: EdgeInsets.only(
-                          left: totalWidth * 0.1, top: totalHeight * 0.1),
-                      child: SizedBox(
-                        height: 50,
-                        width: totalWidth * 0.8,
-                        child: TextButton(
-                            onPressed: () {
-                              if (_formKey.currentState!.validate()) {
-                                _check();
-                              }
-                            },
-                            style: TextButton.styleFrom(
-                              backgroundColor: Color(0xFFF6AC19),
-                              primary: Colors.white,
-                              shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(15.0),
-                              ),
-                            ),
-                            child: Text('ҮЗЭХ')),
-                      ),
-                    ),
-                  )
                 ],
               ),
             ),
           ),
-        ),
-      )
-    ]);
+          Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: Form(
+              key: _formKey,
+              child: Padding(
+                padding:
+                    EdgeInsets.only(left: totalWidth * 0.1, top: totalHeight * 0.4),
+                child: Container(
+                  margin: EdgeInsets.only(top: 30),
+                  // color: Color.fromARGB(50, 200, 50, 20),
+                  child: Column(
+                    children: <Widget>[
+                      Container(child: Text("Хүйс нь")),
+                      Container(
+                        margin: EdgeInsets.only(top: 10),
+                        child: Padding(
+                          padding: EdgeInsets.only(
+                              left: totalWidth * 0, top: totalHeight * 0),
+                          child: SizedBox(
+                            height: 60,
+                            width: totalWidth * 0.8,
+                            child:
+
+                                /* TextFormField(
+                          keyboardType: TextInputType.number,
+                          decoration: InputDecoration(
+                            hintText: 'Хүйс',
+                            hintStyle: TextStyle(color: Colors.black),
+                            filled: true,
+                            fillColor: Color(0xFFF8FAFB),
+                            enabledBorder: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(15),
+                                borderSide: BorderSide(color: Color(0xFFB4091E))),
+                            focusedBorder: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(15),
+                                borderSide: BorderSide(color: Color(0xFFB4091E))),
+                          ),
+                          cursorColor: Color(0xFF30C6CC),
+                        ),*/
+                                Container(
+                              decoration: BoxDecoration(
+                                border: Border.all(
+                                    width: 1.0, color: Color(0xFFB4091E)),
+                                borderRadius: BorderRadius.all(Radius.circular(
+                                        15.0) //                 <--- border radius here
+                                    ),
+                              ),
+                              child: DropdownButton(
+                                underline: Container(),
+                                hint: _dropDownValue == null
+                                    ? Text('Dropdown')
+                                    : Text(
+                                        _dropDownValue,
+                                        style: TextStyle(color: Colors.blue),
+                                      ),
+                                isExpanded: true,
+                                iconSize: 30.0,
+                                style: TextStyle(color: Colors.blue),
+                                items: ['Эрэгтэй', 'Эмэгтэй'].map(
+                                  (val) {
+                                    return DropdownMenuItem<String>(
+                                      value: val,
+                                      child: Text(val),
+                                    );
+                                  },
+                                ).toList(),
+                                onChanged: (val) {
+                                  setState(
+                                    () {
+                                      _dropDownValue = val as String;
+                                    },
+                                  );
+                                },
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                      Container(
+                        margin: EdgeInsets.only(top: 30),
+                        child: Padding(
+                          padding: EdgeInsets.only(
+                              left: totalWidth * 0.1, top: totalHeight * 0.1),
+                          child: SizedBox(
+                            height: 50,
+                            width: totalWidth * 0.8,
+                            child: TextButton(
+                                onPressed: () {
+                                  if (_formKey.currentState!.validate()) {
+                                    _check();
+                                  }
+                                },
+                                style: TextButton.styleFrom(
+                                  backgroundColor: Color(0xFFF6AC19),
+                                  primary: Colors.white,
+                                  shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(15.0),
+                                  ),
+                                ),
+                                child: Text('ҮЗЭХ')),
+                          ),
+                        ),
+                      ),
+
+                      isThirdApiData?Container(
+                        margin: EdgeInsets.only(top: 10),
+                        child: Text(tailbar_data),):Container(),
+
+                      isThirdApiData?Container(
+                        margin: EdgeInsets.only(top: 10),
+                        child: Text("zasal"),):Container(),
+
+                      isThirdApiData?Container(
+                        margin: EdgeInsets.only(top: 10),
+                        child: Text(zasal_data),):Container()
+
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          )
+        ]),
+      ),
+    );
   }
 
   void _check() async {
@@ -305,6 +326,7 @@ class _ZurhaiState extends State<Zurhai> {
     var res1=await Network().forThirdApi();
 
 
+//json.decode(utf8.decode(data.bodyBytes));
 
     List body1 =await json.decode(res1.body.toString());
 
@@ -314,7 +336,21 @@ if(body1[i]["onsar"].toString()==jsonData['year'].toString()
     &&body1[i]["huis"].toString()==_dropDownValue.toString()
     ){
 
-  print("here is your content tailbar:"+body1[i]["tailbar"].toString());
+  tailbar_data=utf8.decode(body1[i]["tailbar"].runes.toList());
+//var z1=utf8.decode(body1[i]["zasal"].runes.toList());
+  //var zasal=body1[i]["zasal"][0].toString();
+  print ("zasal:"+body1[i]["zasal"].toString());
+  var a=jsonEncode(body1[i]["zasal"]);
+  print("a"+a.toString());
+  Map<String, dynamic> map=json.decode(utf8.decode(a.runes.toList()));
+
+
+zasal_data=map.toString();
+  zasal_data =zasal_data.replaceAll("{", "").replaceAll("}", "");
+
+
+
+ isThirdApiData=true;
 
 
 }else{
